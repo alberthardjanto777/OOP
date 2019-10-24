e841f9e18cf097f37643899dd9a9fac0672c9079
adrianstefanus39 <56949181+adrianstefanus39@users.noreply.github.com>
2019-10-24 12:00:59 +0700

    Add files via upload

---
+++ pertemuan/src/pertemuan/Mahasiswa.java
@@ -0,0 +1,12 @@
+package pertemuan;
+
+public class Mahasiswa {
+	private String nama;
+	private String nim;
+	private int age;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
